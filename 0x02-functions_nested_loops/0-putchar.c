011996fac44f943452f0f5872605a823e460ee84
kaltsegayemihretu <kaltsegayemihretu@gmail.com>
2023-03-21 07:29:22 -0700

    thi sis xommiting

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,22 @@
-include <stdio.h> 
-int main(){
-	printf("_putchar \n")
-}
+#include "main.h"
+	/**
+	 * main - print _putchar
+	 * description: "_putchar function in use"
+	 * Return: 0
+	 */
+
+
+	int main(void)
+	{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+	}
+
