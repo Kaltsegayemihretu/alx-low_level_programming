55b5b51a74027043e3eda34414a2cf8fde5e9ea9
kaltsegayemihretu <kaltsegayemihretu@gmail.com>
2023-03-21 11:56:02 -0700

    commiting

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,20 +3,19 @@
 	 * main - print _putchar
 	 * description: "_putchar function in use"
 	 * Return: 0
-	 */
-	
-int main(void)
+	 */	
+	int main(void)
 	/* main is void function */
 	{
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
+		_putchar('_');
+		_putchar('p');
+		_putchar('u');
+		_putchar('t');
+		_putchar('c');
+		_putchar('h');
+		_putchar('a');
+		_putchar('r');
+		_putchar('\n');
 	return (0);
 	}
 
